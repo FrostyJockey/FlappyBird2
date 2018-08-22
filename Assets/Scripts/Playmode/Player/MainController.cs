cdad0da7cdc366e1263f8bc6bc7e2d5f7b78537f
Bitchboi Pikmin <cdtdcdt@gmail.com>
2018-08-22 15:44:11 -0400

    First commit

---
+++ Assets/Scripts/Playmode/Player/MainController.cs
@@ -0,0 +1,37 @@
+﻿using Assets.Scripts.Playmode.Player;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainController : MonoBehaviour {
+
+    private static bool IsSceneLoaded(string name)
+    {
+        for (var i = 0; i < SceneManager.sceneCount; i++)
+            if (SceneManager.GetSceneAt(i).name == name) return true;
+
+        return false;
+    }
+
+    private static void LoadGame()
+    {
+        if (!IsSceneLoaded(Scenes.Game))
+        {
+            SceneManager.LoadSceneAsync(Scenes.Game, LoadSceneMode.Additive);
+        }
+
+        //La scène active est celle dans laquelle les GameObjects sont créés au runtime.
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName(Scenes.Game));
+    }
+
+    private void Start()
+    {
+        LoadGame();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Playmode/Player/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] private KeyCode up = KeyCode.W;
+    //[SerializeField] private KeyCode left = KeyCode.A;
+    //[SerializeField] private KeyCode down = KeyCode.S;
+    //[SerializeField] private KeyCode right = KeyCode.D;
+    private TranslateMover translateMover;
+
+    private void Awake()
+    {
+        translateMover = GetComponent<TranslateMover>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+        var position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        translateMover.Move(Vector3.right);
+
+        if (Input.GetKeyDown(up))
+        {
+            translateMover.Move(Vector3.up);
+        }
+
+    }
+}
